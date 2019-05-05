b39a8cb71feedb32d459aaa6b87cb9af88e8a829
ljhint <ljhint@qq.com>
2019-05-05 11:11:47 +0800

    first commit

---
+++ js/is_fenxiang.js
@@ -0,0 +1,4 @@
+if(!navigator.userAgent.match(/(iPhone|iPod|Android|ios)/i)){
+document.writeln("<div class=\"fenxiang\"><div style=\"float:left;\">·ÖÏí:</div><div class=\"jiathis_style_24x24\"><a class=\"jiathis_button_qzone\"></a><a class=\"jiathis_button_tsina\"></a><a class=\"jiathis_button_tqq\"></a> <a class=\"jiathis_button_weixin\"></a> <a href=\"../www.jiathis.com/share\" class=\"jiathis jiathis_txt jtico jtico_jiathis\"></a> </div></div>");
+document.writeln(" <script type=\"text/javascript\" src=\"http://v3.jiathis.com/code_mini/jia.js\" charset=\"utf-8\"></script>");
+}
